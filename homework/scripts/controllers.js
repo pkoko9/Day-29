208a194ff0f4c26995685cc6c9c728c2d0df9542
Paul Kokoszyna <pkoko9@hotmail.com>
2015-02-22 23:20:15 -0600

    Menu hide not working - able to post/get images and captions - some styling work still needed

---
+++ homework/scripts/controllers.js
@@ -0,0 +1,100 @@
+
+angular.module ('app.controllers', [])
+
+	.controller('postController', function($scope, $http) {
+
+		$scope.data='';
+		$scope.myArray=[];
+		$scope.menuHide = true;
+
+		$scope.expandMenu = function() {
+		$scope.menuHidden = false;
+		};
+
+
+// get and post image code
+
+	$http.get('http://tiny-pizza-server.herokuapp.com/collections/pk')
+	.success(function(response) {
+		// Successfully received a response from the server
+
+		$scope.myArray = [];
+		for(var i=0; i<response.length; i++) {
+			if(response[i].url && response[i].caption) {
+				$scope.myArray.push(response[i]);
+			}
+		}
+		// $scope.myArray = response;
+		console.log($scope.myArray);
+		console.log(response);
+
+	})
+	.error(function(err) {
+		// Got an error back from the server
+		console.log(err);
+	});
+
+	$scope.cancelClick = function() {
+		$scope.imageURL = '';
+		$scope.caption = '';
+	};
+
+
+	// Called when submit is clicked
+	// Send the data to the server
+	$scope.sendImage = function(myImage, myCaption) {
+		var myImageExists = false;
+		for(var i=0; i<$scope.myArray.length; i++) {
+			if($scope.myArray[i].url === myImage) {
+				myImageExists = true;
+			}
+		}
+
+		if(myImageExists === false) {
+			$http.post(
+				'http://tiny-pizza-server.herokuapp.com/collections/pk',
+				
+				{url: myImage,
+				caption: myCaption}
+			);
+		}
+		
+		else {
+			alert('That image already exists');
+		}
+
+	};
+
+// error message code
+
+	$scope.addImage = function() {
+		'use strict';
+		var correctEntry=0;
+
+		var failArray = [
+			'You must enter a valid URL for your image',
+			'Image URL must begin with http:// or https://',
+			'You must enter an image desciption'
+			];
+	
+		if(angular.isUndefined($scope.imageURL) || $scope.imageURL === "") {
+			$scope.fail1 = failArray[0];
+		} else {
+			$scope.fail1 = undefined;
+		}
+		if($scope.imageURL.substring(0,7) === "http://" || $scope.imageURL.substring(0,8)==="https://") {
+				$scope.fail1 = "";
+			} else {
+				$scope.fail1 = failArray[1];
+			}
+
+		if(angular.isUndefined($scope.caption) || $scope.caption === "") {
+			$scope.fail2 = failArray[2];
+		} else {
+			$scope.fail2 = "";
+		}
+
+		$scope.imageURL = '';
+		$scope.caption = '';
+	}; 
+});
